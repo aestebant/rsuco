d29c7abb6e7e1f5efa7c987777ddcd2c81fd32fe
aurora <aurora.est.tosc@gmail.com>
2018-06-13 13:49:55 +0200

    Changes in maven dependendies

--- src/main/java/subjectreco/core/RunEval.java
+++ src/main/java/subjectreco/core/RunEval.java
@@ -1,19 +1,14 @@
 package subjectreco.core;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.google.common.base.Preconditions;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.mahout.cf.taste.model.DataModel;
-
-import com.google.common.base.Preconditions;
-
 import subjectreco.evaluator.IEvaluator;
-import subjectreco.evaluator.SplitEvaluator;
+import subjectreco.evaluator.KFoldEvaluator;
 import subjectreco.util.ConfigLoader;
-import subjectreco.util.IConfiguration;
 import subjectreco.util.ModelManage;
+
+import java.io.File;
 
 public class RunEval {
 
@@ -30,22 +25,23 @@ public class RunEval {
 		Configuration config = ConfigLoader.XMLFile(new File(args[1]));
 
 		DataModel model = mm.loadModel("ratings");
-		IEvaluator eval = new SplitEvaluator();
-		//IEvaluator eval = new KFoldEvaluator();
-		if (eval instanceof IConfiguration)
-			((IConfiguration) eval).configure(config.subset("subjectreco.evaluator"));
+
+		//IEvaluator eval = new SplitEvaluator();
+		IEvaluator eval = new KFoldEvaluator();
+
+		eval.configure(config.subset("evaluator"));
 		
 		eval.setRecommenderBuilder(new File(args[2]), mm);
 		eval.setDataModel(model);
 		
 		eval.setOrderedbyNPrefsSubjects(mm);
-		//eval.execute();
-		List<Long> seeds = new ArrayList<>(5);
+		eval.execute();
+
+		/*List<Long> seeds = new ArrayList<>(5);
 		seeds.add(10L);
 		seeds.add(20L);
 		seeds.add(30L);
-		
-		eval.execute(seeds);
+		eval.execute(seeds);*/
 	}
 
 }
