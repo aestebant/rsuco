f64e4222edcec28fb66788e41f232322eced6ded
Aurora Esteban Toscano <i32estoa@uco.es>
2018-07-10 12:21:31 +0200

    Reach and time computation added

--- src/main/java/subjectreco/core/Run.java
+++ src/main/java/subjectreco/core/Run.java
@@ -1,6 +1,7 @@
 package subjectreco.core;
 
 import java.io.File;
+import java.util.List;
 
 import org.apache.commons.configuration2.Configuration;
 import org.apache.mahout.cf.taste.common.TasteException;
@@ -9,6 +10,7 @@ import org.apache.mahout.cf.taste.model.DataModel;
 
 import com.google.common.base.Preconditions;
 
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import subjectreco.recommender.IRecommender;
 import subjectreco.util.ConfigLoader;
 import subjectreco.util.ModelManage;
@@ -20,8 +22,8 @@ import subjectreco.util.RecommenderLoader;
  */
 class Run {
 	
-	static int nRecommendations = 3;
-	static long userID = -1;
+	private static int expectedRecos = 3;
+	private static long userID = -1;
 
 	public static void main(String[] args) {
 		Preconditions.checkArgument(args.length == 2, "Use: <bd configuration.xml> <rs configuration.xml>");
@@ -41,18 +43,23 @@ class Run {
 		DataModel model = mm.loadModel("ratings");
 
 		long start = System.nanoTime();
-
+		int obtainedRecos = 0;
+		int nUsers = 0;
 
 		recommender.execute(model);
 
-		System.out.println("User | Recommendations");
+		// Show recommendations
+		System.out.println("User --> Recommendations");
 		if (userID == -1) {
 			LongPrimitiveIterator users = null;
 			try {
 				users = recommender.getRecommender().getDataModel().getUserIDs();
 				while (users.hasNext()) {
 					long id = users.nextLong();
-					System.out.println(id + " --> " + recommender.getRecommender().recommend(id, nRecommendations));
+					List<RecommendedItem> recommendations = recommender.getRecommender().recommend(id, expectedRecos);
+					obtainedRecos += recommendations.size();
+					nUsers ++;
+					System.out.println(id + " --> " + recommendations);
 				}
 			} catch (TasteException e) {
 				e.printStackTrace();
@@ -61,13 +68,19 @@ class Run {
 		}
 		else {
 			try {
-				System.out.println(userID + " --> " + recommender.getRecommender().recommend(userID, nRecommendations));
+				System.out.println(userID + " --> " + recommender.getRecommender().recommend(userID, expectedRecos));
+				nUsers ++;
 			} catch (TasteException e) {
 				e.printStackTrace();
 			}
 		}
 
+		// Show statistics
 		double time = (System.nanoTime() - start) * 1e-9;
-		System.out.println("Needed time (s): " + time);
+		double reach = (double) obtainedRecos / (expectedRecos * nUsers) * 100.0;
+		System.out.println();
+		System.out.println("Needed time (s):\t" + time);
+		System.out.println("Average time per user (s):\t" + time / nUsers);
+		System.out.println("Reach of RS (%):\t" + reach);
 	}
 }
